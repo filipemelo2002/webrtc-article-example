c28e74fcf26aa86cdaa44b3f5701302c08b5232a
Filipe Melo <filipe.silva@encora.com>
2024-03-12 14:40:50 -0300

    feature: implement webRTC service

---
+++ client/src/webrtc.service.ts
@@ -0,0 +1,38 @@
+export class WebRTCService {
+  private peerConnection: RTCPeerConnection;
+
+  constructor() {
+    const configuration: RTCConfiguration = {
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    };
+    this.peerConnection = new RTCPeerConnection(configuration);
+  }
+
+  async makeOffer() {
+    const offer = await this.peerConnection.createOffer();
+    return offer;
+  }
+
+  async makeAnswer() {
+    const answer = await this.peerConnection.createAnswer();
+    return answer;
+  }
+
+  async setRemoteOffer(offer: RTCSessionDescriptionInit) {
+    await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+  }
+
+  async setLocalOffer(offer: RTCSessionDescriptionInit) {
+    await this.peerConnection.setLocalDescription(offer);
+  }
+  
+  async getMediaStream() {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
+    mediaStream.getTracks().forEach((track) => {
+      this.peerConnection.addTrack(track, mediaStream);
+    });
+    return mediaStream;
+  }
+}
