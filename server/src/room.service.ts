387bd759b28f73af77a1e37a166e5364ed4c229c
Filipe Melo <filipe.silva@encora.com>
2024-03-12 09:18:41 -0300

    feature: implement room service in backend

--- server/src/room.service.ts
+++ server/src/room.service.ts
@@ -1,20 +1,76 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 interface Room {
   owner: string;
-  guest?: string;
-};
+  guest: string | null;
+}
 
 export class RoomService {
   rooms = new Map<string, Room>();
 
-  constructor(private server: Server) {
-  }
+  constructor(private server: Server) {}
 
   initialize() {
     this.server.on("connection", (socket) => {
-      console.log("user connected", socket.id)
+      this.handleOnCreateRoom(socket);
+      this.handleOnJoinRoom(socket);
+      this.handleOnSendOffer(socket);
+      this.handleOnSendAnswer(socket);
+    });
+  }
+
+  handleOnCreateRoom(socket: Socket) {
+    socket.on("room/create", (payload) => {
+      const { roomName } = payload;
+      this.rooms.set(roomName, {
+        owner: socket.id,
+        guest: null,
+      });
+    });
+  }
+
+  handleOnJoinRoom(socket: Socket) {
+    socket.on("room/join", (payload) => {
+      const {roomName} = payload;
+      
+      const room = this.rooms.get(roomName);
+      
+      if (!room) {
+        return;
+      }
+      room.guest = socket.id;
+      socket.to(room.owner).emit("user/joined");
     })
   }
 
+  handleOnSendOffer(socket: Socket) {
+    socket.on("offer/send", (payload) => {
+      const { roomName, offer } = payload;
+
+      const room = this.rooms.get(roomName);
+      
+      if (!room || !room.guest) {
+        return;
+      }
+      socket.to(room.guest).emit("offer/receive", {
+        offer
+      })
+    })
+  }
+
+  handleOnSendAnswer(socket: Socket) {
+    socket.on("answer/send", (payload) => {
+      const { answer, roomName } = payload;
+
+      const room = this.rooms.get(roomName);
+
+      if (!room) {
+        return;
+      }
+
+      socket.to(room.owner).emit("answer/receive", {
+        answer
+      })
+    })
+  }
 }
